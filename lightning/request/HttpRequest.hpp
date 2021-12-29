21bcf561fd2b9194abf87afe573129b59f0cd8e0
IshaySela <ishaisela@gmail.com>
2021-12-29 12:32:13 +0200

    Moved the request parsing from HeadersParser to HttpRequest::createRequest.

--- lightning/request/HttpRequest.hpp
+++ lightning/request/HttpRequest.hpp
@@ -14,7 +14,17 @@ namespace lightning
         using HeadersMap = std::unordered_map<std::string, std::string>;
         HeadersMap headers;
 
-        static auto getNextToken(std::string line, std::string del, int &outIndex, int offset = 0) -> std::string;        
+        /**
+         * @brief Parse the line and retrive every character from the offset until reaching the delimiter.
+         *
+         * @param line The string to get the substr from.
+         * @param del The delimiter to find.
+         * @param outIndex Output variable that will contain the index of the found delimiter. If the delimiter was not found, it will be the length of line.
+         * @param offset The offset character to start from.
+         * @return std::string The substring from offset to del.
+         */
+        static auto getNextToken(std::string line, std::string del, int &outIndex, int offset = 0) -> std::string;
+
     public:
         HttpRequest(std::string method, std::string rawUri, std::string protocolVersion, HeadersMap &headers);
 
@@ -40,7 +50,23 @@ namespace lightning
             std::string protocolVersion;
         };
 
+        /**
+         * @brief Parse the request line sent by the client.
+         * The request line should be as specified in https://datatracker.ietf.org/doc/html/rfc2616#section-5.1.
+         * General request line will be structured as follows: Method SP Request-URI SP HTTP-Version CRLF
+         *
+         * @param requestLine The request line to parse.
+         * @return HttpRequest::RequestLine An object that contains the method, request uri and http version.
+         */
         static auto parseRequestLine(std::string requestLine) -> HttpRequest::RequestLine;
-        static auto parseHeaders(std::string headers) -> HeadersMap;
+
+        /**
+         * @brief Parse a string that is structured as specified in the rfc https://datatracker.ietf.org/doc/html/rfc2616#section-4.2.
+         * General headers sent by the client will be structured as follows: Key ": " Value CRLF Key ": " Value ...
+         *
+         * @param rawHeaders The raw headers string.
+         * @return HeadersMap An object that maps string keys to string values.
+         */
+        static auto parseHeaders(std::string rawHeaders) -> HeadersMap;
     };
 }
