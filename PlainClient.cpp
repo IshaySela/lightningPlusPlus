34860bace057014c0303c16f9cd991ac4630399f
Ishay Sela <ishaisela@gmail.com>
2026-05-15 22:22:28 +0300

    added plain client, stream and server for none https support

---
+++ PlainClient.cpp
@@ -0,0 +1,11 @@
+#include "lightning/PlainClient.hpp"
+
+namespace lightning
+{
+    PlainClient::PlainClient(int fd) : fd(fd), plainStream(fd) {}
+
+    auto PlainClient::getStream() -> stream::PlainStream&
+    {
+        return this->plainStream;
+    }
+}
